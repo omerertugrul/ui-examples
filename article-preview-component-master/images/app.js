ec3d158d78cff8966e73ec1ae522689afcfba623
Ömer Ertuğrul Güverçin <97853371+omerertugrul@users.noreply.github.com>
2022-05-15 15:52:11 +0300

    Delete article-preview-component-master directory

--- article-preview-component-master/images/app.js
+++
@@ -1,8 +0,0 @@
-const share = document.querySelector(".share");
-const popup = document.querySelector(".popup");
-
-share.addEventListener("click", function () {
-  share.classList.toggle("active");
-  popup.classList.toggle("active");
-
-});
